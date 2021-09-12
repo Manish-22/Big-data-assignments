bbb4b082398222b869f51e95b2d0a0adf6424543
manish-22 <manishreddym22@gmail.com>
2021-09-12 14:01:01 +0530

    Fixed Code

--- Task 1/reducer.py
+++ Task 1/reducer.py
@@ -4,26 +4,6 @@ import sys
 
 hourcount= {}
 
-# for line in sys.stdin:
-#     line = line.strip()
-#     hour, count = line.split('\t')
-#     try:
-#         count = int(count)
-#     except ValueError:
-#         continue
-
-#     if current_hour == hour:
-#         current_count += count
-#     else:
-#         if current_hour:
-#             print(int(current_hour), " ", current_count, sep="")
-#         current_count = count
-#         current_hour = hour
-
-# # do not forget to output the last hour if needed!
-# if current_hour == hour:
-#     print(int(current_hour), " ", current_count, sep="")
-
 for line in sys.stdin:
     hour, count = line.split("\t")
     hour = hour.strip()
