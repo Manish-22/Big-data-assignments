bb6b3d4df792ed9eae9373b59224114470e1b9f8
manish-22 <manishreddym22@gmail.com>
2021-09-12 18:32:02 +0530

    add reducer

--- Task2/reducer.py
+++ Task2/reducer.py
@@ -4,14 +4,23 @@ import sys
 
 statecity = {}
 
+pres_state=""
+pres_city=""
+state_count=0
+city_count=0
 for line in sys.stdin:
     state, city = line.split(" ")
     state = state.strip()
     city = city.strip()
 
-    if state not in statecity.keys():
-        statecity[state] = 0
-    statecity[state] += city
+    if pres_state =="" or pres_state!=state:
+        print(state,state_count)
+        pres_state=state
+        state_count=0
+    state_count+=1
+    if pres_city =="" or pres_city!=city:
+        print(city,city_count)
+        pres_city=city
+        city_count=0
+    city_count+=1
 
-for state, city in statecity.items():
-    print("{} {}".format(int(state), city))
