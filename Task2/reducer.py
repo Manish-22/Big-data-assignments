772df882514f385d8fd43e0beedb01ad1818042d
manish-22 <manishreddym22@gmail.com>
2021-09-12 19:06:37 +0530

    Updated reducer

--- Task2/reducer.py
+++ Task2/reducer.py
@@ -6,22 +6,30 @@ statecity = {}
 
 pres_state=""
 pres_city=""
-state_count=""
-city_count=""
+state_count=0
+city_count=0
 for line in sys.stdin:
     state, city = line.split("\t")
     state = state.strip()
     city = city.strip()
 
-    if pres_state =="" or pres_state!=state:
-        print(state,state_count)
+    if pres_state =="":
+        print(state)
         pres_state=state
         state_count=0
-    state_count+=1
-    if pres_city =="" or pres_city!=city:
-        if pres_city!=city:
-            print(city,city_count)
+    if pres_city =="":
+        pres_city=city
+    if pres_city!=city:
+        print(pres_city,city_count)
         pres_city=city
         city_count=0
     city_count+=1
+    if pres_state!=state:
+        print(pres_state,state_count)
+        print(state)
+        pres_state=state
+        state_count=0
+    state_count+=1
+print(city,city_count)
+print(state,state_count)
 
