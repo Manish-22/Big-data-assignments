76b400074b699abd7b0dda80bcbd9c30d2fc0d20
manish-22 <manishreddym22@gmail.com>
2021-09-12 13:59:26 +0530

    Fixed Code

--- Task 1/mapper.py
+++ Task 1/mapper.py
@@ -1,22 +1,19 @@
-import sys 
+#!/usr/bin/env python
+
+import sys
 import json
 from datetime import datetime as dt
-count=0
+t = dict()
+
 for line in sys.stdin:
-    line =line.strip()
-    
-    data_dict = json.loads(line)
-
-    if(any(x in data_dict["Description"] for x in ["lane blocked", "shoulder blocked","overturned vehicle"]) and data_dict["Severity"]>=2 and data_dict["Sunrise_Sunset"]=="Night" and data_dict["Visibility(mi)"]<=10 and data_dict["Precipitation(in)"]>= 0.2 and any(y in data_dict["Weather_Condition"] for y in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers" ,"Blowing Dust"])):
-        time1=dt.strptime(data_dict["Start_Time"],"%Y-%m-%d %H:%M:%S")
-        time2=dt.strptime(data_dict["End_Time"],"%Y-%m-%d %H:%M:%S")
-							
-        start_hour=int(time1.__str__()[-8:-6])
-        end_hour=int(time2.__str__()[-8:-6])
-
-        if(end_hour-start_hour>1):
-            print("start Hour",start_hour)
-        #print(data_dict["Weather_Condition"])
-        print(start_hour)
-        #print("count",count)
-        count+=1
+    idk = line.strip()
+    one_rec = json.loads(line)
+    if(any(x in one_rec["Description"].lower() for x in ["lane blocked", "shoulder blocked", "overturned vehicle"]) and 
+        type(one_rec["Description"]) != float and 
+        type(one_rec["Weather_Condition"]) != float and
+        one_rec["Severity"] >= 2 and one_rec["Sunrise_Sunset"] == "Night" and 
+        one_rec["Visibility(mi)"] <= 10.0 and 
+        one_rec["Precipitation(in)"] >= 0.2 and 
+        any(y == one_rec["Weather_Condition"] for y in ["Heavy Snow", "Thunderstorm", "Heavy Rain", "Heavy Rain Showers", "Blowing Dust"])):
+        time1 = one_rec["Start_Time"][11:13]
+        print('%s\t%s' % (time1, 1))
---
+++ Task 1/reducer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+
+hourcount= {}
+
+# for line in sys.stdin:
+#     line = line.strip()
+#     hour, count = line.split('\t')
+#     try:
+#         count = int(count)
+#     except ValueError:
+#         continue
+
+#     if current_hour == hour:
+#         current_count += count
+#     else:
+#         if current_hour:
+#             print(int(current_hour), " ", current_count, sep="")
+#         current_count = count
+#         current_hour = hour
+
+# # do not forget to output the last hour if needed!
+# if current_hour == hour:
+#     print(int(current_hour), " ", current_count, sep="")
+
+for line in sys.stdin:
+    hour, count = line.split("\t")
+    hour = hour.strip()
+    count = count.strip()
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    if hour not in hourcount.keys():
+        hourcount[hour] = 0
+    hourcount[hour]+= count
+
+for hour, count in hourcount.items():
+    print("{} {}".format(int(hour), count))
