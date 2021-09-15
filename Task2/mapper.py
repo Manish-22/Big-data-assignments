d59b348c75d79b1a3868b349978157fe71c12193
Soupy710 <swaroopbhat710@gmail.com>
2021-09-15 20:14:14 +0530

    hope

--- Task2/mapper.py
+++ Task2/mapper.py
@@ -30,4 +30,4 @@ for line in sys.stdin:
             r = requests.post(url, data=json.dumps(data),
                               headers=headers).json()
 
-            print(r["state"], r["city"])
+            print(r["state"], r["city"], sep="\t")
