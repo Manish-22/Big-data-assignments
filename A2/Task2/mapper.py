e0842c79c0426f65c977b0fc466bc9846d04b318
manish-22 <manishreddym22@gmail.com>
2021-10-19 22:06:17 +0530

    remove constructor

--- A2/Task2/mapper.py
+++ A2/Task2/mapper.py
@@ -13,16 +13,15 @@ v_path = sys.argv[1]
 embedded_path = sys.argv[2]
 
 f = open(embedded_path)
-sim_matrix = list()
+sim_matrix = []
 data = json.load(f)
 for i in range(len(data.items())):
 	sim_matrix.append([0 for j in range(len(data.items()))])
 for f1,f2 in data.items():
-    temp = list()
-    s1 = [i*i for i in f2]   # [1,2,3,4] ==> [1,4,8,16] 
+    temp = []
+    s1 = [i*i for i in f2]   
     mag1 = math.sqrt(sum(s1))
     for f11,f22 in data.items():	
-    	# print(f1,f11)
     	s2 = [i*i for i in f22]
     	mag2 = math.sqrt(sum(s2))
     	temp = [(f2[i] * f22[i]) for i in range(len(f2))]
