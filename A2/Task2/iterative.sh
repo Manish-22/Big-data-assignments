b951294f434302b4a9a93b05f9b155dcdfdaee49
GauravJayaraj <gauravjayaraj@gmail.com>
2021-10-24 19:30:02 +0530

    fixed ig mapper portal closed, we need to submit

--- A2/Task2/iterative.sh
+++ A2/Task2/iterative.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 CONVERGE=1
 ITER=1
-rm v v1 log*
+rm v.txt v1.txt log*
 
 $HADOOP_HOME/bin/hadoop dfsadmin -safemode leave
 hdfs dfs -rm -r /Assignment-2/output/task-* 
 
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.2.2.jar \
--mapper "'/home/pes_bigdata/Desktop/Assignment-2/mapper_t1.py'" \
--reducer "'/home/pes_bigdata/Desktop/Assignment-2/reducer_t1.py' '/home/pes_bigdata/Desktop/Assignment-2/v'" \
--input /Assignment-2/Input/dataset-sample.txt \
+-mapper "'/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task1/mapper.py'" \
+-reducer "'/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task1/reducer.py' '/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task2/v.txt'" \
+-input /Assignment-2/Input/dataset-sample2.txt \
 -output /Assignment-2/output/task-1-output
 
 while [ "$CONVERGE" -ne 0 ]
 do
 	echo "############################# ITERATION $ITER #############################"
 	hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.2.2.jar \
-	-mapper "'/home/pes_bigdata/Desktop/Assignment-2/mapper_t2.py' '/home/pes_bigdata/Desktop/Assignment-2/v' '/home/pes_bigdata/Desktop/Assignment-2/Input/node-map-sample.json'" \
-	-reducer "'/home/pes_bigdata/Desktop/Assignment-2/reducer_t2.py'" \
+	-mapper "'/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task2/mapper.py' '/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task2/v.txt' '/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task2/embedding-sample2.json'" \
+	-reducer "'/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task2/reducer.py'" \
 	-input /Assignment-2/output/task-1-output/part-00000 \
 	-output /Assignment-2/output/task-2-output
-	touch v1
-	hadoop fs -cat /Assignment-2/output/task-2-output/part-00000 > "/home/pes_bigdata/Desktop/Assignment-2/v1"
+	touch v1.txt
+	hadoop fs -cat /Assignment-2/output/task-2-output/part-00000 > "/home/pes1ug19cs599/Desktop/Big-data-assignments/A2/Task2/v1.txt"
 	CONVERGE=$(python3 check_conv.py $ITER>&1)
 	ITER=$((ITER+1))
 	hdfs dfs -rm -r /Assignment-2/output/task-2-output/
