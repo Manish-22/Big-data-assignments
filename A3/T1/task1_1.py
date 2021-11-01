d700984c19f00125ba89cb06381cdfba849910dc
GauravJayaraj <gauravjayaraj@gmail.com>
2021-11-01 22:56:18 +0530

    task1_1 updated2

--- A3/T1/task1_1.py
+++ A3/T1/task1_1.py
@@ -1,6 +1,10 @@
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
+
+from pyspark.sql.types import FloatType
+
 import sys
+
 sc=SparkContext.getOrCreate()
 sqlContext = SQLContext(sc)
 
@@ -21,13 +25,20 @@ rdd = rdd.filter(lambda x:x!=header)
 base_df = rdd.toDF(header)
 base_df.show(20)
 
+base_df = base_df.withColumn("AverageTemperature",base_df.AverageTemperature.cast(FloatType()))
+
 country_df = base_df.filter(base_df["Country"] == country)
 country_df.show(5)
 
-avg_ofavg_city = country_df.groupby("City").count()
+#count_ofavg_city = country_df.groupby("City").count()
 
-avg_ofavg_city.show(10)
+#count_ofavg_city.show(10)
 
 #df_grp = df.groupBy("author").count().orderBy("count", ascending=0).show(10)
 
-aggr_avg_city = country_df
+#country_df.AverageTemperature = country_df.AverageTemperature.cast(FloatType())
+
+sum_ofavg_city = country_df.groupBy("City").avg("AverageTemperature")
+sum_ofavg_city.show(10)
+
+ 
